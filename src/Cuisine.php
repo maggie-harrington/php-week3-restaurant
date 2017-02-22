fdd639032c2c3ef2d1006f746f9d120021838779
Benjamin T. Seaver and Maggie Harrington <benjamin@>
2017-02-22 11:36:24 -0800

    add empty Cuisine class

---
+++ src/Cuisine.php
@@ -0,0 +1,64 @@
+<?php
+    class Cuisine
+    {
+        private $id;
+        private $name;
+        private $link;
+
+        function __construct()
+        {
+
+        }
+
+        function getId()
+        {
+
+        }
+
+        function getName()
+        {
+
+        }
+
+        function getLink()
+        {
+
+        }
+
+        function setId()
+        {
+
+        }
+
+        function setName()
+        {
+
+        }
+
+        function setLink()
+        {
+
+        }
+
+        function save()
+        {
+
+        }
+
+        function update()
+        {
+
+        }
+
+        function delete()
+        {
+
+        }
+
+        static function getAll()
+        {
+
+        }
+    }
+
+?>
