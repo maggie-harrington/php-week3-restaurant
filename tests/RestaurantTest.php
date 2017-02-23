2efd1faace8a39937262ffd9865167edcbc8f464
Benjamin T. Seaver and Maggie Harrington <maggie@>
2017-02-23 08:22:48 -0800

    Add new test for delete (tests/RestaurantTest.php)

--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -43,22 +43,31 @@ class RestaurantTest extends PHPUnit_Framework_TestCase
         $this->assertEquals([$new_restaurant3, $new_restaurant2], $all_restaurants);
     }
 
-    // function test_delete_Cuisine()
-    // {
-    //     // Arrange
-    //     $new_cuisine = new Cuisine('Pub', 'link1');
-    //     $new_cuisine2 = new Cuisine('Korean', 'link2');
-    //
-    //     // Act
-    //     $new_cuisine->save();
-    //     $new_cuisine->delete();
-    //     $new_cuisine2->save();
-    //     $all_cuisines = Cuisine::getAll();
-    //
-    //     // Assert
-    //     $this->assertEquals([$new_cuisine2], $all_cuisines);
-    // }
-    //
+    function test_delete_Restaurant()
+    {
+      // Arrange
+      $new_cuisine = new Cuisine('Pub', 'link1');
+      $new_cuisine->save();
+      $new_restaurant = new Restaurant('Sasquatch', 'Link_Sas', 'Hillsdale', $new_cuisine->getId());
+      $new_restaurant->save();
+
+      $new_cuisine2 = new Cuisine('Fast Food', 'google.com?fastfood');
+      $new_cuisine2->save();
+      
+      $new_restaurant2 = new Restaurant('McDonalds', 'mcdonalds.com?location=23222424', 'NE Portland', $new_cuisine2->getId());
+      $new_restaurant2->save();
+
+      $new_restaurant3 = new Restaurant('BurgerKing', 'bk.com?location=232424', 'NE Portland', $new_cuisine2->getId());
+      $new_restaurant3->save();
+
+      // Act
+      $new_restaurant2->delete();
+      $all_restaurants = Restaurant::getAll($new_cuisine2->getId());
+
+      // Assert
+      $this->assertEquals([$new_restaurant3], $all_restaurants);
+    }
+
     // function test_update_Cuisine()
     // {
     //     // Arrange
